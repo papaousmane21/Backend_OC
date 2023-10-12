02f4d7fcce4bc5296ff4864ffef15dd0314a3914
papaousmane21 <papaousmanediene98@gmail.com>
2023-10-12 13:27:12 +0200

    Implementation du CRUD sur le ficher App

--- app.js
+++ app.js
@@ -37,6 +37,18 @@ app.use((req, res, next) => {
       .catch(error => res.status(404).json({ error }));
   });
 
+  app.put('/api/stuff/:id', (req, res, next) => {
+    Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+      .catch(error => res.status(400).json({ error }));
+  });
+
+  app.delete('/api/stuff/:id', (req, res, next) => {
+    Thing.deleteOne({ _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+      .catch(error => res.status(400).json({ error }));
+  });
+
   app.get('/api/stuff', (req, res, next) => {
     Thing.find()
       .then(things => res.status(200).json(things))
